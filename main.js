f8b2257c39fc08a363151106b2eb2c888b3d9d0b
James Chinweike <chinweikechukwudi@gmail.com>
2022-10-20 15:13:53 +0100

    commit

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var button = document.querySelector('.btn');
-var view = documment.querySelector('.outcome');
+var btn = document.querySelector('.btn');
+var view = document.querySelector('.outcome');
 
 
 btn.addEventListener('click', palindrome);
 
 function palindrome(){
-    var input = document.querySelector('#text');
+    var input = document.querySelector('#text').value;
+    let length = input.length;
+
+    let start = input.substring(0, Math.floor(length / 2)).
+    toLowerCase();
+    alert(start);
+
     
 }
